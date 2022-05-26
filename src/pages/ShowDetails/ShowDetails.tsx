60db839cfb9d2cb26c26707c861c98b0c332562a
Hien Pham <thehienpv@gmail.com>
2022-05-26 22:59:42 +0700

    add back button

--- src/pages/ShowDetails/ShowDetails.tsx
+++ src/pages/ShowDetails/ShowDetails.tsx
@@ -1,20 +1,39 @@
-import React, { FC } from "react";
-import { useLocation } from "react-router-dom";
-import { Card } from "../../components";
-import { EspisodeModel } from "../../types/espisode.model";
-const Espisode: FC = () => {
-  const location = useLocation();
-  const espisode = location.state as EspisodeModel;
+import React, { FC } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
+import { ReactComponent as IconBack } from '../../assets/images/back-icon.svg'
+import { Card } from '../../components'
+import { EpisodeModel } from '../../types/episode.model'
 
+const Episode: FC = () => {
+  const location = useLocation()
+  const episode = location.state as EpisodeModel
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>Episode Detail</h1>
+      <Heading>
+        <IconBack onClick={() => {
+          navigate(-1)
+        }}/>
+        <h2>Episode Detail</h2>
+      </Heading>
       <Card
-        name={espisode?.name}
-        summary={espisode?.summary || ""}
-        image={espisode.image}
+        name={episode?.name}
+        summary={episode?.summary || ''}
+        image={episode.image}
       />
     </div>
-  );
-};
-export default Espisode;
+  )
+}
+
+const Heading = styled.div`
+  display: flex;
+  align-items: center;
+  & svg {
+    width: 20px;
+    margin-right: 10px;
+    cursor: pointer;
+  }
+`
+
+export default Episode
